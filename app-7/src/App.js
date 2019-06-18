daf5ecd3e7531bcb2f5cddd5b587f07387219383
jj2delgado <jj2delgado@yahoo.com>
2019-06-18 17:32:08 -0600

    in progress

--- app-7/src/App.js
+++ app-7/src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Todo from "./Todo"
+import NewTask from "./NewTask"
+import List from "./List"
 
 class App extends Component {
+  constructor(){
+    super()
+
+    this.state ={
+      list: []
+    }
+  }
   render() {
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Your to-do List</h1>
+        <List />
+        <NewTask />
       </div>
     );
   }
---
+++ app-7/src/NewTask.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react'
+
+class NewTask extends Component{
+    render(){
+        return(
+            <div></div>
+        )
+    }
+}
+export default NewTask
